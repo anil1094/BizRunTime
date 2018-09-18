cfe591de35cd7fd7a5c3becbee7422fd81b376f6
Anil Singh <anils15896@gmail.com>
2018-09-18 21:05:12 +0530

    intial commit

---
+++ EntityProgram/NorthwindControllerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class NorthwindControllerController : Controller
+    {
+        NorthWindContext db = new NorthWindContext();
+        public ActionResult Index()
+        {
+            db.CategoriesTable.ToList();
+            db.ProductsTable.ToList();
+            return View();
+        }
+    }
+}
