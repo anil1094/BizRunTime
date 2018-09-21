6d5254fbfdc948c36151a669711dcd65bcc6356a
Anil Singh <anils15896@gmail.com>
2018-09-21 19:43:49 +0530

    initial commit

---
+++ MVC/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ViewResult Index()
+        {
+            
+            ViewData["key"] = "anil";
+            ViewData["products"] = new List<string>()
+            {
+                "svmsung TV",
+                "nike causal"
+            };
+
+            ViewBag.name = "David";
+            ViewBag.category = new List<string>()
+            {
+                "electronic",
+                "shoes"
+            };
+            return View();
+        }
+    }
+}
