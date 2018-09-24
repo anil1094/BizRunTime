3ed7bac2ef3f2a5798ed3abcf90cd06e6a9612f4
Anil Singh <anils15896@gmail.com>
2018-09-24 20:32:22 +0530

    first commit

---
+++ WEB Api/HomeController1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSimple1.Models;
+
+namespace WebApiSimple1.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public StudentModel GetStudent()
+        {
+            return new StudentModel
+            {
+                Name = "Sandeep",
+                Age = 27
+            };
+        }
+    }
+}
